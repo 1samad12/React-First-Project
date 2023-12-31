7b4ca7748b06a22a4ba65723b1bd641d837f335c
1samad12 <samad123450099@gmail.com>
2023-12-31 21:03:14 +0500

    'first-project-for-react-js'

---
+++ src/component/Button.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+export const Buttons = () => {
+    return (
+        <> 
+        <Button fullWidth id="fullWidth" style={{marginTop:'10px' , padding:'10px 40px'}} variant="contained">
+       Submit From
+      </Button>
+        </>
+    )
+}
